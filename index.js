57caa0a1798e58cc3bc552d972238efc1d00ea5e
KshitizAgarwal21 <kshitizagarwal27@gmail.com>
2023-03-23 21:52:54 +0530

    api added

--- index.js
+++ index.js
@@ -11,20 +11,21 @@ app.listen(8080, (err) => {
   }
   console.log("Server started successfully");
 });
-
+const loginCredentials = {
+  username: "Letsupgrade",
+  password: "password@123",
+};
+const userDetails = {
+  FullName: "John Lester",
+  Username: "Letsupgrade",
+  DOB: "09001999",
+  City: "Bangalore",
+  Email: "student@letsupgarde.io",
+  Phone: "9999999999",
+  avatar:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPoACnzH9x8JQxfPZg9YCZqTLd6JRRDeYKbH7OFYeQiw&usqp=CAU&ec=48665701",
+};
 app.post("/login", (req, res) => {
-  const loginCredentials = {
-    username: "Letsupgrade",
-    password: "password@123",
-  };
-  const userDetails = {
-    FullName: "Student Letsupgrade",
-    Username: "Letsupgrade",
-    DOB: "09001999",
-    City: "Bangalore",
-    Email: "student@letsupgarde.io",
-    Phone: "9999999999",
-  };
   const { username, password } = req.body;
 
   if (username === loginCredentials.username) {
@@ -33,5 +34,20 @@ app.post("/login", (req, res) => {
 
       res.status(200).send(token);
     }
+  }
+});
+
+app.post("/removedp", (req, res) => {
+  var token = req.headers.authorization;
+  var decoded = jwt.verify(token, mysalt);
+  console.log(decoded);
+  if (decoded) {
+    userDetails.avatar = "";
+    var updatedToken = jwt.sign(userDetails, mysalt);
+    res
+      .status(200)
+      .send({ msg: "DP removed successfully", token: updatedToken });
+  } else {
+    res.status(401).send({ msg: "Unauthorised request" });
   }
 });
